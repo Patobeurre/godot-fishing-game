9bf5ccb9c7d0591e77f6b59ca6c8f2d74bd0497b
Patobeurre <brice@zaclys.net>
2024-11-02 16:08:50 +0100

    desk drawer interaction

--- scripts/desk.gd
+++ scripts/desk.gd
@@ -1,12 +1,42 @@
 extends Node3D
 
 
-@onready var interact_body = $Cube_022/StaticBody3D
+@onready var desk_body = $Cube_022/StaticBody3D
+@onready var drawer_body = $Cube_128/StaticBody3D
+@onready var drawer_mesh = $Cube_128
+
+var drawer_pos_offset :float = 0.4
+var is_drawer_opened :bool = false
+var is_interacting :bool = false
 
 
 func _ready() -> void:
-	interact_body.interact_performed.connect(interact)
+	desk_body.interact_performed.connect(interact_desk)
+	drawer_body.interact_performed.connect(interact_drawer)
 
 
-func interact():
+func interact_desk():
 	UiManager.open("DocumentsCollection")
+
+
+func interact_drawer():
+	if is_interacting: return
+	
+	is_interacting = true
+	var target_pos :Vector3 = drawer_mesh.position
+	
+	if is_drawer_opened:
+		target_pos.x += drawer_pos_offset
+		drawer_body.interact_text = "Open"
+	else:
+		target_pos.x -= drawer_pos_offset
+		drawer_body.interact_text = "Close"
+	
+	var tween := get_tree().create_tween().bind_node(drawer_mesh).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(drawer_mesh, "position", target_pos, 0.5)
+	tween.play()
+	
+	await tween.finished
+	
+	is_drawer_opened = not is_drawer_opened
+	is_interacting = false
